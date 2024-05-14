5f889a583ecb8074281c08fee948e9c507f656b7
dacdepchai <93912537+dacdepchai@users.noreply.github.com>
2024-05-14 09:20:24 +0700

    create model

--- src/main/java/com/example/springbootinternz/Friend.java
+++ src/main/java/com/example/springbootinternz/Friend.java
@@ -1,4 +1,21 @@
 package com.example.springbootinternz;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+
 public class Friend {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String department;
+    private String email;
+    private String country;
 }
