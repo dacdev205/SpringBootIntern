474d58502570a185b70dc803e720d30500c1ffca
dacdepchai <93912537+dacdepchai@users.noreply.github.com>
2024-05-14 09:20:52 +0700

    create service

---
+++ src/main/java/com/example/springbootinternz/FriendService.java
@@ -0,0 +1,28 @@
+package com.example.springbootinternz;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class FriendService {
+    @Autowired
+    private FriendRepository friendRepository;
+
+    public List<Friend> getFriends() {
+        return friendRepository.findAll();
+    }
+
+    public void addFriend(Friend friend) {
+        friendRepository.save(friend);
+    }
+
+    public void updateFriend(Friend friend) {
+        friendRepository.save(friend);
+    }
+    public void deleteFriend(Integer id) {
+        friendRepository.deleteById(id);
+    }
+}
+
